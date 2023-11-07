08071814128c3ed06bcca204561db68d425d84bc
hoagie195 <hoagie195@gmail.com>
2023-11-07 08:33:34 -0500

    update: Added EMBER raw dataset

--- StaticAV.py
+++ StaticAV.py
@@ -5,36 +5,39 @@ import tensorflow as tf
 from keras import datasets, layers, models, optimizers
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+import json
+import sys
 
-peh_data = pd.read_csv('PE_Header.csv')
+train_data = pd.read_json('ember2018/train_features_0.jsonl', lines=True).sample(frac=0.01, random_state=1)
 
-peh_data.info()
+x_data = train_data.iloc[:, 6:].values # extract all columns except the first six
 
-x_data = peh_data.iloc[:, 2:].values # extract all columns except the first two
-
-y_data = peh_data.iloc[:, 1].values # extract first column for Type
+y_data = train_data.iloc[:, 3].values # extract first column for Type
 
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3)
 
-# Normalization
 scaler = MinMaxScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
 model = models.Sequential()
-model.add(layers.Normalization(input_shape = [52,], axis = None))
-model.add(layers.Dense(2, activation='sigmoid', activity_regularizer=tf.keras.regularizers.L2(0.01)))
-model.add(layers.Dense(1, activation = 'sigmoid', activity_regularizer=tf.keras.regularizers.L2(0.01)))
+model.add(layers.Normalization(input_shape=[500,], axis=None))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
 model.summary()
 
-adam = optimizers.Adam(learning_rate=0.3)
+adam = optimizers.Adam(learning_rate=0.001)
 
-model.compile(optimizer='adam',
+model.compile(optimizer=adam,
               loss=tf.keras.losses.binary_crossentropy,
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train, epochs=300,
-                    validation_data=(x_test, y_test))
+                    validation_data=(x_test, y_test),
+                    batch_size=32)
 
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
