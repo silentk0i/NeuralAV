d79fecefb837e55c21a49ae9598cda7d7267ef6d
DrejCosby <90070950+DrejCosby@users.noreply.github.com>
2023-11-30 17:51:15 -0500

    update: added vectorize file

---
+++ vectorize.py
@@ -0,0 +1,9 @@
+import ember
+
+def main():
+
+    ember.create_vectorized_features("C:\\Users\\Colin\\Desktop\\project\\ember2018")
+    ember.create_metadata("C:\\Users\\Colin\\Desktop\\project\\ember2018")
+
+if __name__ == "__main__":
+    main()
